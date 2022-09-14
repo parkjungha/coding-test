f57e52de82a0f6e4f8d04e3a61ba7bbe8f956a3b
parkjungha <jungha4709@naver.com>
2022-09-14 22:23:31 +0900

    week11

---
+++ week11/두 큐 합 같게 만들기.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def solution(queue1, queue2):
+    q1 = deque(queue1)
+    q2 = deque(queue2)
+    sum1 = sum(q1)
+    sum2 = sum(q2)
+
+    for i in range(len(q1)*3): # 최대 반복 횟수(q1 길이 * 3)
+        if sum1 == sum2:
+            return i
+        if sum1 > sum2:
+            num = q1.popleft()
+            q2.append(num)
+            sum1 -= num
+            sum2 += num
+        else:
+            num = q2.popleft()
+            q1.append(num)
+            sum2 -= num
+            sum1 += num
+    
+    return -1
