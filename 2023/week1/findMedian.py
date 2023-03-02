5fa33de48727e1f2e2cbddb7bd9e6e10c9424b86
parkjungha <jungha4709@naver.com>
2023-03-02 14:23:00 +0900

    week2

--- 2023/week1/findMedian.py
+++ 2023/week1/findMedian.py
@@ -19,3 +19,15 @@ class Solution:
             elif i > 0 and nums1[i-1] > nums2[j]:
                 imax = i - 1
             else:
+                if i == 0: max_of_left = nums2[j-1]
+                elif j == 0: max_of_left = nums1[i-1]
+                else: max_of_left = max(nums1[i-1], nums2[j-1])
+
+                if (m + n) % 2 == 1:
+                    return max_of_left
+
+                if i == m: min_of_right = nums2[j]
+                elif j == n: min_of_right = nums1[i]
+                else: min_of_right = min(nums1[i], nums2[j])
+
+                return (max_of_left + min_of_right) / 2.0
