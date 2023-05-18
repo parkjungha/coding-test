39a61d421d24fa991f96adc7e4011aec8344aa98
parkjungha <jungha4709@naver.com>
2023-05-18 19:36:01 +0900

    w

--- 2023/week13/3_pacific.py
+++ 2023/week13/3_pacific.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Runtime 58.74% (282 ms) Memory 34.1% (17.9MB)
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         def bfs(queue, visited):
             dx = [-1,1,0,0]
@@ -10,7 +11,7 @@ class Solution:
                     nx = x + dx[i]
                     ny = y + dy[i] 
                     if 0<= nx <r and 0<= ny <c and not visited[nx][ny]:
-                        if heights[nx][ny] >= heights[x][y]:
+                        if heights[nx][ny] >= heights[x][y]: # 물이 흐를 수 있으면
                             queue.append([nx,ny])
                             visited[nx][ny] = 1
                         
@@ -20,9 +21,11 @@ class Solution:
 
         pacific_visited = [[0]*c for _ in range(r)]
         pacific_q = deque()
+
         atlantic_visited = [[0]*c for _ in range(r)]
         atlantic_q = deque()
 
+        # 경계에 있는 행,열들 담아줌
         for i in range(r): # 첫번째 열, 마지막 열
             pacific_q.append([i,0])
             atlantic_q.append([i,c-1])
@@ -34,6 +37,7 @@ class Solution:
         bfs(pacific_q, pacific_visited)
         bfs(atlantic_q, atlantic_visited)
 
+        # 모든 좌표에 대해 둘다 만족하는 좌표값이 정답
         ans = []
         for i in range(r):
             for j in range(c):
